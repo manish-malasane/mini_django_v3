2cb97c55c4f895f8be40e0f92dee52b461796e65
Manish Malasane <mmalasane6@gmail.com>
2023-04-04 15:59:22 +0530

    new business logic added

--- jobs/views.py
+++ jobs/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from jobs.models import Portal
+from django.shortcuts import get_object_or_404
+from jobs.models import Portal, JobDescription, Applicant, JobTitle
 
 # Create your views here.
 
 
+# TODO  - write an API endpoint to get list of portals (.../jobs/portal_details)
+
 def get_portal_details(request):
-    portals = Portal.objects.order_by()
+    portals = Portal.objects.order_by("id")
     # how to get URL associated with django-view
 
     from django.urls import reverse
@@ -20,6 +23,39 @@ def get_portal_details(request):
     return JsonResponse(por_, safe=False)
 
 
+# TODO  - write an API endpoint to get description of 1 job (.../jobs/job_detail/job_id)
 
+def get_detail_job_desc(request, job_id):
+    foo = get_object_or_404(JobDescription, pk=job_id)
+    return render(request, "jobs/job_description.html", {"job_desc": foo})
+
+
+# TODO  - write an API endpoint to get list of applicants (.../jobs/applicants_details)
+
+def get_applicants(request):
+    applicants = []
+    foo = Applicant.objects.order_by("id")
+    for i in foo:
+        applicants.append(i.name)
+    return JsonResponse(applicants, safe=False)
+
+
+# TODO  - write an API endpoint to get details of single applicant (.../jobs/applicant/1)
+
+
+def get_applicant_details(request, id_):
+    bar = get_object_or_404(Applicant, pk=id_)
+    return render(request, "jobs/applicant_desc.html", {"appli_desc": bar})
+
+
+# TODO  - write an API endpoint to get list of titles (.../jobs/jobtitles)
+
+def get_titles(request):
+    titles = []
+    result = JobTitle.objects.order_by("id")
+
+    for i in result:
+        titles.append(i.title)
+    return JsonResponse(titles, safe=False)
 
 
