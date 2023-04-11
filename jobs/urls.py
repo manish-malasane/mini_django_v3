e60e9e81eccd260d036ec269759d3480182f74b5
Manish Malasane <mmalasane6@gmail.com>
2023-04-11 10:26:42 +0530

    classbased views using DRF APIView

--- jobs/urls.py
+++ jobs/urls.py
@@ -17,32 +17,55 @@ Including another URLconf
 from django.urls import path
 from . import views
 from . import views2
+from . import views3
 
 urlpatterns = [
+    # Views file endpoints url mapping #
+
     path("job_detail/<int:job_id>", views.get_detail_job_desc, name="get_job_detail"),
     path(
         "applicant/<int:id_>", views.get_applicant_details, name="get_applicant_details"
     ),
     path("portals/", views.Portals.as_view(), name="portals"),
     path("titles/", views.Titles.as_view(), name="titles"),
+
+    # Views2 file endpoints url mapping using django generic views #
+
+    # 1) Applicants
+
     path("v2/applicants/", views2.ApplicantList.as_view(), name="applicant-list"),
     path("v2/applicants/create/", views2.ApplicantCreate.as_view(), name="applicant-create"),
     path("v2/applicants/detail/<int:pk>", views2.ApplicantDetail.as_view(), name="applicant-detail"),
     path("v2/applicants/update/<int:pk>", views2.ApplicantUpdate.as_view(), name="applicant-update"),
     path("v2/applicants/delete/<int:pk>", views2.ApplicantDelete.as_view(), name="applicant-delete"),
+
+    # 2) Portals
+
     path("v2/portals/", views2.PortalList.as_view(), name="portal-list"),
     path("v2/portals/create/", views2.PortalCreate.as_view(), name="portal-create"),
     path("v2/portals/detail/<int:pk>", views2.PortalDetail.as_view(), name="portal-detail"),
     path("v2/portals/update/<int:pk>", views2.PortalUpdate.as_view(), name="portal-update"),
     path("v2/portals/delete/<int:pk>", views2.PortalDelete.as_view(), name="portal-delete"),
+
+    # 3) JobTitles
+
     path("v2/jobtitles/", views2.JobTitleList.as_view(), name="job-title-list"),
     path("v2/jobtitles/create/", views2.JobTitleCreate.as_view(), name="job-title-create"),
     path("v2/jobtitles/detail/<int:pk>", views2.JobTitleDetail.as_view(), name="job-title-detail"),
     path("v2/jobtitles/update/<int:pk>", views2.JobTitleUpdate.as_view(), name="job-title-update"),
     path("v2/jobtitles/delete/<int:pk>", views2.JobTitleDelete.as_view(), name="job-title-delete"),
+
+    # 4) JobDescriptions
+
     path("v2/jd/", views2.JobDescriptionList.as_view(), name="job-description-list"),
     path("v2/jd/create/", views2.JobDescriptionCreate.as_view(), name="job-description-create"),
     path("v2/jd/detail/<int:pk>", views2.JobDescriptionDetail.as_view(), name="job-description-detail"),
     path("v2/jd/update/<int:pk>", views2.JobDescriptionUpdate.as_view(), name="job-description-update"),
     path("v2/jd/delete/<int:pk>", views2.JobDescriptionDelete.as_view(), name="job-description-delete"),
+
+    # Views3 file endpoints url mapping using django-rest-framework APIView class #
+
+    path("v3/portals/", views3.Portals.as_view(), name="portals"),
+    path("v3/users/", views3.UserList.as_view(), name="users"),
+
 ]
---
+++ jobs/views3.py
@@ -0,0 +1,30 @@
+import json
+
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from jobs.models import Portal
+
+
+class Portals(APIView):
+    def get(self, request):
+        portals = Portal.objects.all()
+        final = dict()
+        for portal in portals:
+            final[portal.id] = {"name": portal.name, "description": portal.description}
+        return Response(final)
+
+    def post(self, request):
+        data = json.loads(request.body)
+        portal = Portal.objects.create(**data)
+        return Response("portal created")
+
+
+class UserList(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        final = dict()
+        for user in users:
+            final[user.id] = {"id": user.id, "username": user.username, "email": user.email}
+        return Response(final)
+
